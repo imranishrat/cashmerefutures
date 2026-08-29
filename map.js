d7abcf1de12b5fe8a28580e80449ca072d68c259
Imran Ishrat <imranishrat@gmail.com>
2026-08-29 20:00:43 +1200

    Delete map.js

--- map.js
+++
@@ -1,7 +0,0 @@
-// Cashmere Global Map data.
-// The map currently uses a curated set of cashmere-producing countries.
-// Production figures are intentionally not shown until comparable, current data are established.
-const CASHMERE_MAP_COUNTRIES = [
-  'China', 'Mongolia', 'Iran', 'Afghanistan', 'India',
-  'Kyrgyzstan', 'Kazakhstan', 'Pakistan', 'Nepal'
-];
